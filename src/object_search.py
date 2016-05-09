dea691cab330656d49ede84eac5f4a4927b96b84
kate.tolstaya@gmail.com <kate.tolstaya@gmail.com>
2016-05-09 11:53:53 -0700

    opencv part started

--- src/object_search.py
+++ src/object_search.py
@@ -14,9 +14,13 @@ import math
 import cv2
 from sensor_msgs.msg import CompressedImage
 import numpy as np
+from matplotlib import pyplot as plt
 import threading
 import copy
 import rospkg
+
+
+MIN_MATCH_COUNT = 10
 
 
 #-------------------------------------------------------------------------------
@@ -129,12 +133,71 @@ class ObjectSearch:
       print "Capturing image"
       self.image = copy.deepcopy(cv_image)
       self.processImage = False
+      for target_name in self.trainImageNames:
+        target_image = cv2.imread(self.trainImageDir + '/' + target_name+'.png',0)
+        if (match_img(self,target_image, cv_image) == True):
+          print "Object found: "+target_name 
+        else:
+          print "Object NOT found: "+target_name 
 
     # Show image
-    cv2.imshow("Image live feed", cv_image)
+    #cv2.imshow("Image live feed", cv_image)
     cv2.waitKey(1)
 
     self.lock.release()
+
+  #---------------------------
+  # OpenCV Matching algorithm
+  def match_img (self, img1, img2):
+    # img1 - box
+    # img2 - box in scene
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h,w = img1.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.CV_AA)
+
+        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+        img3 = drawMatches(self, img1,kp1,img2,kp2,good,None,**draw_params)
+
+        plt.imshow(img3, 'gray'),plt.show()
+        return True;
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        matchesMask = None
+        return False;
 
   #-------------------------------------------------------------------------------
   # Capture image, display it and save it to the debug folder
